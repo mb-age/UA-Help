394575a95431aabfb153de1052266683f1ac9b6c
bartosz <bartosz.cholewa@lightcode.eu>
2022-03-09 16:43:31 +0100

    admin - setting up profile admin view

--- uahelp_app/admin.py
+++ uahelp_app/admin.py
@@ -3,8 +3,20 @@ from . import models
 
 
 # Register your models here.
+@admin.register(models.Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'account_type', 'user', 'country')
+    fieldsets = (
+        ("Informacje podstawowe", {
+            'fields': ('name', 'user')
+        }),
+        ("Pierdoły", {
+            'fields': ('account_type', 'country')
+        }),
+    )
+    ordering = ('-name',)
 
-admin.site.register(models.Profile)
+
 admin.site.register(models.AccountType)
 admin.site.register(models.Post)
 admin.site.register(models.Link)
