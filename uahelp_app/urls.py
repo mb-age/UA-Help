e86915a68978569fb2f5a70864284eb4ee65c93c
mb-age <i.m.angel.777@gmail.com>
2022-03-15 18:40:11 +0100

    deleted rest of pycache files, updated views, created templates, created folder for static files, created urls file in app folder, created url for home page, profile, post details

---
+++ uahelp_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.HomePageView.as_view(), name='home_page'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail_view'),
+]
+
--- uahelp_app/views.py
+++ uahelp_app/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+'''
+no na pewno strona główna (z listą postów), strona formularza profilu (edycja), strona profilu, strona posta, strona formularza do postu, logowanie, rejestracja formularz, informacja o zarejestrowaniu/wyslaniu maila aktywującego, podziękowanie za kliknięcie w link?, zmiana hasla, reset hasla, dodaj post( przekierowanie na strone logowania jesli niezalogowany, else strona z formularzem), strona z powiadomieniem o weryfikacji, akcja - mail o pozytywnej weryfikacji, 
+'''
+
+class HomePageView(TemplateView):
+    template_name = 'uahelp_app/home.html'
+
+class ProfileView(TemplateView):
+    template_name = 'uahelp_app/profile.html'
+
+class ProfileUpdateView():
+    pass
+
+class PostDetailView(TemplateView):
+    template_name = 'uahelp_app/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        pass
+
+class PostCreateView():
+    pass
+
+class PostUpdateView():
+    pass
+
+class LogInView():
+    pass
+
+class RegisterView():
+    pass
+
+class RegisterConfirmationView(TemplateView):
+    pass
+
+class VerificationInfoView(TemplateView):
+    pass
+
+class PasswordUpdateView():
+    pass
+
+class PasswordResetView():
+    pass
+
+
+
+
+
